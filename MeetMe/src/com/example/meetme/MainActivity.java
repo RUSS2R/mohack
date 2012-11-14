af57452c010e8f105e3b1e272029ab5779dd7de0
RU$$ <RU$$@134.61.89.158>
2012-11-15 00:13:53 +0100

    Retrieve all users from server

--- MeetMe/src/com/example/meetme/MainActivity.java
+++ MeetMe/src/com/example/meetme/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.meetme;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	Intent mapActivityIntent;
+	EditText myStatus;
 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mapActivityIntent = new Intent(this,MapsActivity.class);
+        myStatus = (EditText) findViewById(R.id.editText_myStatus);
+        
+        
+        
+        Button shareButton = (Button) findViewById(R.id.button_share_location);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				mapActivityIntent.putExtra("MyStatus", myStatus.getText().toString());				
+				startActivity(mapActivityIntent);
+			}
+		});
     }
 
     @Override
@@ -17,4 +39,6 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    
 }
